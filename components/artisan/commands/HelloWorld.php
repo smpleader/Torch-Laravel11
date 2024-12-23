37ec020dc617c0dbf8922ba944886852db49509e
Pham Minh <smpleader@gmail.com>
2024-12-23 02:32:26 -0800

    test with artisan

--- components/artisan/commands/HelloWorld.php
+++ components/artisan/commands/HelloWorld.php
@@ -7,7 +7,7 @@ class HelloWorld extends Command
     protected $signature = 'hello:world';
     protected $description = 'This is a simple hello world';
 
-    public function handle()
+    public function handle(): void
     {
         $this->comment('Hello World');
     }
